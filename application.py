c16e8fa00830eb2923eb4ec924931a2906b5d050
William Do <william@ultimaspin.com>
2018-11-18 19:11:03 +0000

    add new url resource for player

--- application.py
+++ application.py
@@ -10,3 +10,6 @@ def league():
         "divisions": ["Division 1", "Division 2", "Division 3"]
     })
 
+@app.route("/player", methods=["GET"])
+def player():
+    return jsonify({"firstName": "Joe", "lastName": "Bloggs"})
