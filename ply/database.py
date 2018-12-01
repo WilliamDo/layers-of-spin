72ad6a7ea14de3effa19271e8aed128e6bfdd951
William Do <william@ultimaspin.com>
2018-12-01 21:10:35 +0000

    create single connection only for database

--- ply/database.py
+++ ply/database.py
@@ -1,40 +1,21 @@
 import psycopg2
 import psycopg2.extras
 
-# conn = psycopg2.connect(dbname="ply", user="postgres", password="docker", host="localhost")
-# cur = conn.cursor()
-# cur.execute("SELECT * FROM league")
-# data = cur.fetchone()
-#
-# print(data)
-#
-# cur.close()
-# conn.close()
+# TODO what if this connection is dropped? how to reconnect!?
+conn = psycopg2.connect(dbname="ply", user="postgres", password="docker", host="localhost")
 
 
 def create_player(player):
-    conn = psycopg2.connect(dbname="ply", user="postgres", password="docker", host="localhost")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO player (first_name, last_name) VALUES (%s, %s)", (player["first_name"], player["last_name"]))
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("INSERT INTO player (first_name, last_name) VALUES (%s, %s)", (player["first_name"], player["last_name"]))
 
-    conn.commit()
 
-    cur.close()
-    conn.close()
-
-def get_player(id):
-    conn = psycopg2.connect(dbname="ply", user="postgres", password="docker", host="localhost")
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute("SELECT * FROM player where id = %s", (id,))
-    data = cur.fetchone()
-    cur.close()
-    conn.close()
-    return data
+def get_player(player_id):
+    with conn.cursor(cursor_factory=psycopg2.extras.DictCursor) as curs:
+        curs.execute("SELECT * FROM player where id = %s", (player_id,))
+        data = curs.fetchone()
+        curs.close()
+        return data
 
 # create_player({"first_name": "Joe", "last_name": "Bloggs"})
-#
-# player = get_player(4)
-#
-# print(player["id"])
-# print(player["first_name"])
-# print(player["last_name"])
