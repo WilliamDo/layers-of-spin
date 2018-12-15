7fd103e092eb5b6f9769ace6a3b2133bf8e74cd5
William Do <william@ultimaspin.com>
2018-12-15 19:59:57 +0000

    add some default data for database

--- db/init_ply.sql
+++ db/init_ply.sql
@@ -8,3 +8,43 @@ CREATE TABLE league (
   id SERIAL PRIMARY KEY,
   league_name VARCHAR
 );
+
+CREATE TABLE fixture (
+  id SERIAL PRIMARY KEY,
+  home_team VARCHAR,
+  away_team VARCHAR
+);
+
+CREATE TABLE match (
+  id SERIAL PRIMARY KEY,
+  fixture_id INTEGER REFERENCES fixture(id),
+  home_player_id INTEGER REFERENCES player(id),
+  away_player_id INTEGER REFERENCES player(id)
+);
+
+CREATE TABLE match_game (
+  match_id INTEGER REFERENCES match(id),
+  game_order INTEGER,
+  home_score INTEGER,
+  away_score INTEGER
+);
+
+DO $$
+DECLARE
+  v_fixture_id INT;
+  v_home_player_id INT;
+  v_away_player_id INT;
+BEGIN
+  INSERT INTO player (first_name, last_name) VALUES ('Timo', 'Boll')
+  RETURNING id INTO v_home_player_id;
+
+  INSERT INTO player (first_name, last_name) VALUES ('Vladimir', 'Samsonov')
+  RETURNING id INTO v_away_player_id;
+
+  INSERT INTO fixture (home_team, away_team) VALUES ('Team A', 'Team B')
+  RETURNING id INTO v_fixture_id;
+
+  INSERT INTO match (fixture_id, home_player_id, away_player_id) VALUES (v_fixture_id, v_home_player_id, v_away_player_id);
+END $$;
+
+
