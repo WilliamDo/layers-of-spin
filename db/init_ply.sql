e7e378df502f18f1e0debbe9de432689c7fde747
William Do <william@ultimaspin.com>
2018-12-17 21:00:07 +0000

    remove table for games and replace with json fields in the match table

--- db/init_ply.sql
+++ db/init_ply.sql
@@ -19,14 +19,9 @@ CREATE TABLE match (
   id SERIAL PRIMARY KEY,
   fixture_id INTEGER REFERENCES fixture(id),
   home_player_id INTEGER REFERENCES player(id),
-  away_player_id INTEGER REFERENCES player(id)
-);
-
-CREATE TABLE match_game (
-  match_id INTEGER REFERENCES match(id),
-  game_order INTEGER,
-  home_score INTEGER,
-  away_score INTEGER
+  away_player_id INTEGER REFERENCES player(id),
+  home_score JSONB,
+  away_score JSONB
 );
 
 DO $$
@@ -36,21 +31,22 @@ DECLARE
   v_away_player_id INT;
   v_match_id INT;
 BEGIN
-  INSERT INTO player (first_name, last_name) VALUES ('Timo', 'Boll')
+  INSERT INTO player (first_name, last_name)
+  VALUES ('Timo', 'Boll')
   RETURNING id INTO v_home_player_id;
 
-  INSERT INTO player (first_name, last_name) VALUES ('Vladimir', 'Samsonov')
+  INSERT INTO player (first_name, last_name)
+  VALUES ('Vladimir', 'Samsonov')
   RETURNING id INTO v_away_player_id;
 
-  INSERT INTO fixture (home_team, away_team) VALUES ('Team A', 'Team B')
+  INSERT INTO fixture (home_team, away_team)
+  VALUES ('Team A', 'Team B')
   RETURNING id INTO v_fixture_id;
 
-  INSERT INTO match (fixture_id, home_player_id, away_player_id) VALUES (v_fixture_id, v_home_player_id, v_away_player_id)
+  INSERT INTO match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_home_player_id, v_away_player_id, '[11, 11, 11]', '[7, 8, 9]')
   RETURNING id INTO v_match_id;
 
-  INSERT INTO match_game (match_id, game_order, home_score, away_score) VALUES (v_match_id, 1, 11, 7);
-  INSERT INTO match_game (match_id, game_order, home_score, away_score) VALUES (v_match_id, 2, 11, 8);
-  INSERT INTO match_game (match_id, game_order, home_score, away_score) VALUES (v_match_id, 3, 11, 9);
 END $$;
 
 
