3a79d7da01a09f4fd8241ced64d0527ed5cdb241
William Do <william@ultimaspin.com>
2019-02-24 13:03:07 +0000

    add json support

--- src/main/java/com/ultimaspin/Main.kt
+++ src/main/java/com/ultimaspin/Main.kt
@@ -1,17 +1,35 @@
 package com.ultimaspin
 
 import io.ktor.application.*
+import io.ktor.features.ContentNegotiation
 import io.ktor.http.*
+import io.ktor.jackson.jackson
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import org.jdbi.v3.core.Jdbi
 
 fun main(args: Array<String>) {
+
+    val jdbi = Jdbi.create("jdbc:postgresql://localhost/ply?user=ply&password=docker")
+    val dao = Dao(jdbi)
+    val repo = FixtureRepo(dao)
+
     val server = embeddedServer(Netty, 8080) {
+        install(ContentNegotiation) {
+            jackson {
+            }
+        }
         routing {
             get("/") {
                 call.respondText("Hello, world!", ContentType.Text.Html)
+            }
+            get("/snippets") {
+                call.respond(mapOf("OK" to true))
+            }
+            get("/fixture") {
+                call.respond(repo.getFixture(1))
             }
         }
     }
