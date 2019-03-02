3ab449464ff745b4e924137960977be9729672c9
William Do <william@ultimaspin.com>
2019-03-02 13:10:02 +0000

    add additional matches in example fixture

--- db/init_ply.sql
+++ db/init_ply.sql
@@ -63,12 +63,14 @@ DECLARE
   v_home_player_2_id INT;
   v_f_home_player_2_id INT;
   v_home_player_3_id INT;
+  v_f_home_player_3_id INT;
 
   v_away_player_1_id INT;
   v_f_away_player_1_id INT;
   v_away_player_2_id INT;
   v_f_away_player_2_id INT;
   v_away_player_3_id INT;
+  v_f_away_player_3_id INT;
 
   v_home_team_id INT;
   v_away_team_id INT;
@@ -77,12 +79,14 @@ DECLARE
   v_f_away_team_id INT;
 BEGIN
 
+  -- TEAMS --
   INSERT INTO team (team_name) VALUES ('Team A') RETURNING id INTO v_home_team_id;
   INSERT INTO team (team_name) VALUES ('Team B') RETURNING id INTO v_away_team_id;
 
   INSERT INTO fixture_team (team_id) VALUES (v_home_team_id) RETURNING id INTO v_f_home_team_id;
   INSERT INTO fixture_team (team_id) VALUES (v_away_team_id) RETURNING id INTO v_f_away_team_id;
 
+  -- PLAYERS --
   INSERT INTO player (first_name, last_name)
   VALUES ('Timo', 'Boll')
   RETURNING id INTO v_home_player_1_id;
@@ -103,6 +107,11 @@ BEGIN
   VALUES ('Jun', 'Mizutani')
   RETURNING id INTO v_away_player_2_id;
 
+  INSERT INTO player (first_name, last_name)
+  VALUES ('Patrick', 'Chila')
+  RETURNING id INTO v_away_player_3_id;
+
+  -- FIXTURE --
   INSERT INTO fixture (fixture_date, home_team_id, away_team_id)
   VALUES ('2018-12-19', v_home_team_id, v_away_team_id)
   RETURNING id INTO v_fixture_id;
@@ -116,7 +125,8 @@ BEGIN
   RETURNING id INTO v_f_home_player_2_id;
 
   INSERT INTO fixture_player (fixture_team_id, player_id)
-  VALUES (v_f_home_team_id, v_home_player_3_id);
+  VALUES (v_f_home_team_id, v_home_player_3_id)
+  RETURNING id INTO v_f_home_player_3_id;
 
   INSERT INTO fixture_player (fixture_team_id, player_id)
   VALUES (v_f_away_team_id, v_away_player_1_id)
@@ -127,13 +137,38 @@ BEGIN
   RETURNING id INTO v_f_away_player_2_id;
 
   INSERT INTO fixture_player (fixture_team_id, player_id)
+  VALUES (v_f_away_team_id, v_away_player_3_id)
+  RETURNING id INTO v_f_away_player_3_id;
+
+  INSERT INTO fixture_player (fixture_team_id, player_id)
   VALUES (v_f_away_team_id, v_away_player_3_id);
 
   INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
   VALUES (v_fixture_id, v_f_home_player_1_id, v_f_away_player_1_id, '[11, 11, 11]', '[7, 8, 9]');
 
   INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_1_id, v_f_away_player_2_id, '[11, 11, 11]', '[7, 8, 9]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_1_id, v_f_away_player_3_id, '[11, 11, 11]', '[7, 8, 9]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_2_id, v_f_away_player_1_id, '[11, 5, 11, 11]', '[7, 11, 3, 5]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
   VALUES (v_fixture_id, v_f_home_player_2_id, v_f_away_player_2_id, '[11, 5, 11, 11]', '[7, 11, 3, 5]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_2_id, v_f_away_player_3_id, '[11, 5, 11, 11]', '[7, 11, 3, 5]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_3_id, v_f_away_player_1_id, '[11, 5, 11, 10]', '[7, 11, 13, 12]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_3_id, v_f_away_player_2_id, '[11, 5, 11, 10]', '[7, 11, 13, 12]');
+
+  INSERT INTO fixture_match (fixture_id, home_player_id, away_player_id, home_score, away_score)
+  VALUES (v_fixture_id, v_f_home_player_3_id, v_f_away_player_3_id, '[11, 5, 11, 10]', '[7, 11, 13, 12]');
 END $$;
 
 
